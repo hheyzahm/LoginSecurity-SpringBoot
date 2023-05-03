528d8e8e786c4b15c1c3ed2ab92909669b6e7807
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-05-03 15:01:24 +0500

    Initial commit

---
+++ src/main/java/springboot/login_security/LoginSecurityApplication.java
@@ -0,0 +1,13 @@
+package springboot.login_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/login_security/LoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.login_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
